050f315d2431fc7555ca993fee31e7203ea3b617
enoch-strok <enochstrok@outlook.com>
2019-10-29 19:42:54 -0700

    first commit for lab02, remote repo

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+console.log('proof of life!');
+
+var userLike = prompt("do you like stranger things?")
+
+if(userLike.toLowerCase() === "yes" || userLike === "y") {
+console.log("I like it too");
+} else {
+console.log("You should check it out!");
+}
+
+
