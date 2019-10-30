86766b477506f25b19251d39d2d34655fa61a190
enoch-strok <enochstrok@outlook.com>
2019-10-29 20:50:09 -0700

    Added the structure to about me site, just the text

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
 
 console.log('proof of life!');
 
-var userLike = prompt("do you like stranger things?")
+// var userLike = prompt("do you like stranger things?")
 
-if(userLike.toLowerCase() === "yes" || userLike === "y") {
-console.log("I like it too");
-} else {
-console.log("You should check it out!");
-}
+// if(userLike.toLowerCase() === "yes" || userLike === "y") {
+// console.log("I like it too");
+// } else {
+// console.log("You should check it out!");
+// }
 
-TextDecoderStream
+
 
 
