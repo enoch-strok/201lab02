7903c6b1ebe89d4775465b6032161349e84b3797
enoch-strok <enochstrok@outlook.com>
2019-10-30 19:12:28 -0700

    Completed the JS portion of lab02

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 
 console.log('proof of life!');
 
+//--------------------------------------------------//
+//-----------------In Class Example-----------------//
+//--------------------------------------------------//
 // var userLike = prompt("do you like stranger things?")
 
 // if(userLike.toLowerCase() === "yes" || userLike === "y") {
@@ -10,47 +13,76 @@ console.log('proof of life!');
 // console.log("You should check it out!");
 // }
 
-//Questions To Be Asked To User
-var questionUserName = "What is your name?";
-var questionOneBornLocation = "Is Enoch's country of birth Latvia?";
-var questionTwoConstructionIndustryYears = "Did Enoch work in the Construction Industry for 5 years?";
-var questionThreeWhoPaidForUdemyClasses = "Did Enoch pay for his own Udemy classes?";
-var questionFourEmployerClients = "Did Enoch's employers become his clients?";
+//---------------------------------------------------------------//
+//-----------------Questions To Be Asked To User-----------------// [DONE]
+//---------------------------------------------------------------//
+var questionUserName = "What is your name?"
+
+var questionOneBornLocation = "Is Enoch's country of birth Latvia?"
+var questionTwoConstructionIndustryYears = "Did Enoch work in the Construction Industry for 5 years?"
+var questionThreeWhoPaidForUdemyClasses = "Did Enoch pay for his own Udemy classes?"
+var questionFourEmployerClients = "Did Enoch's employers become his clients?"
 var questionFiveGoal = "Is Enoch's goal to make a project/task management web app in the future?"
 
-//Responses to Answers (Correct & Incorrect)
-var answerOneBornLocationCorrect = "";
-var answerOneBornLocationIncorrect = "";
+//----------------------------------------------------------------------------//
+//-----------------Responses to Answers (Correct & Incorrect)-----------------// [DONE]
+//----------------------------------------------------------------------------//
+var answerOneBornLocationCorrect = "Yes, that's correct! Enoch was born in Latvia."
+var answerOneBornLocationIncorrect = "Incorrect! Enoch was born in Latvia, the answer was 'Yes'."
 
-var answerTwoConstructionIndustryYearsCorrect = "";
-var answerTwoConstructionIndustryYearsIncorrect = "";
+var answerTwoConstructionIndustryYearsCorrect = "Yes, That's correct! Enoch worked in the Construction Industry for 5 years."
+var answerTwoConstructionIndustryYearsIncorrect = "Incorrect, Enoch worked in the Construction Industry for 5 years. The correct answer was 'Yes'."
 
-var answerThreeWhoPaidForUdemyClassesCorrect = "";
-var answerThreeWhoPaidForUdemyClassesIncorrect = "";
+var answerThreeWhoPaidForUdemyClassesCorrect = "Correct! Enoch did no pay for this Udemy Classes. His employer paid for it at Enoch's request."
+var answerThreeWhoPaidForUdemyClassesIncorrect = "Incorrect! Enoch did not pay for his own classes. His employer did."
 
-var answerFourEmployerClientsCorrect = "";
-var answerFourEmployerClientsIncorrect = "";
+var answerFourEmployerClientsCorrect = "Correct! Enoch's employers hired him as a contractor and became his clients."
+var answerFourEmployerClientsIncorrect = "Incorrect! Enoch's employers DID become his clients."
 
-var answerFiveGoalCorrect = "";
-var answerFiveGoalIncorrect = "";
+var answerFiveGoalCorrect = "Correct! Enoch's goal is to make a project/task management software in the future"
+var answerFiveGoalIncorrect = "Incorrect! Enoch DOES have a goal to make a project/task management web app."
 
+//--------------------------------------------------------//
+//-----------------Prompts & Console Logs-----------------// [DONE]
+//--------------------------------------------------------//
+var promptUserName = prompt(questionUserName)
 
-//Prompts & Console Logs
-var promptUserName = prompt(questionUserName);
-
-var promptOneBornLocation = prompt(questionOneBornLocation);
+//-----------------BornLocation Prompt, If Statment, Console Log [DONE]
+var promptOneBornLocation = prompt(questionOneBornLocation)
 if(promptOneBornLocation.toLowerCase() === "yes" || promptOneBornLocation === "y"){
-console.log("That's correct!")
+console.log(answerOneBornLocationCorrect)
 } else {
-console.log("That's not correct, the answer was 'Yes', Enoch was born in latvia")
+console.log(answerOneBornLocationIncorrect)
 }
 
-var promptTwoConstructionIndustryYears = prompt(questionTwoConstructionIndustryYears);
+//-----------------TwoConstructionIndustryYears Prompt, If Statment, Console Log [DONE]
+var promptTwoConstructionIndustryYears = prompt(questionTwoConstructionIndustryYears)
+if(promptTwoConstructionIndustryYears.toLowerCase() === "yes" || promptTwoConstructionIndustryYears.toLowerCase() === "y"){
+    console.log(answerTwoConstructionIndustryYearsCorrect)
+    } else {
+    console.log(answerTwoConstructionIndustryYearsIncorrect)  
+    }
 
-var promptThreeWhoePaidForUdemyClasses = prompt(questionThreeWhoPaidForUdemyClasses);
+//-----------------ThreeWhoPaidForUdemyClasses Prompt, If Statment, Console Log [DONE]
+var promptThreeWhoePaidForUdemyClasses = prompt(questionThreeWhoPaidForUdemyClasses)
+if(promptThreeWhoePaidForUdemyClasses.toLowerCase() === "yes" || promptThreeWhoePaidForUdemyClasses.toLowerCase() === "y"){
+    console.log(answerThreeWhoPaidForUdemyClassesCorrect)
+    } else {
+    console.log(answerThreeWhoPaidForUdemyClassesIncorrect)  
+    }
 
-var promptFourEmployerClients = prompt(questionFourEmployerClients);
+//-----------------FourEmployerClients Prompt, If Statment, Console Log [DONE]
+var promptFourEmployerClients = prompt(questionFourEmployerClients)
+if(promptFourEmployerClients.toLowerCase() === "yes" || promptFourEmployerClients.toLowerCase() === "y"){
+    console.log(answerFourEmployerClientsCorrect)
+    } else {
+    console.log(answerFourEmployerClientsIncorrect)  
+    }
 
-var promptFiveGoal = prompt(questionFiveGoal);
-
-//
+//-----------------FiveGoal Prompt, If Statment, Console Log [DONE]
+var promptFiveGoal = prompt(questionFiveGoal)
+if(promptTwoConstructionIndustryYears.toLowerCase() === "yes" || promptTwoConstructionIndustryYears.toLowerCase() === "y"){
+    console.log(answerFiveGoalCorrect)
+    } else {
+    console.log(answerFiveGoalIncorrect)  
+    }
